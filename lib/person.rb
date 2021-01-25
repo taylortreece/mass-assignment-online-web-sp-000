77dd00bc3714576f9ce198d6e131e8fb7405d51d
Taylor Treece <taylor.treece@protonmail.com>
2021-01-25 15:10:02 -0600

    commit tree 740aa32494f819fd9c5cfe4c4bacadfb2c9fe24d

--- lib/person.rb
+++ lib/person.rb
@@ -1,9 +1,15 @@
+# This is how you take unexpected key: value pairs into consideration.
+# No matter how many attributes you're given, they will be given a getter & setter method.
+# With attr_accessor, you hve to account for every single attribute ahead of time. This is abstract.
+# https://www.seancdavis.com/blog/dynamic-attributes-on-instances-in-ruby/
+
 class Person
 
   def initialize(attributes = {})
     attributes.each  do |key, value| 
-      define_singleton_method("#{attr}=") { |val| attributes[attr] = val }
-      define_singleton_method(attr) { attributes[attr] }
+      define_singleton_method("#{key}=") { |val| attributes[key] = val }
+      define_singleton_method(key) { attributes[key] }
+    end
   end
 
 end
