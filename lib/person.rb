271447c246b9a2c17b9d56f35c2853adf19c7ca9
Taylor Treece <taylor.treece@protonmail.com>
2021-01-25 15:04:54 -0600

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,8 +1,9 @@
 class Person
-  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
 
-  def initialize(attributes)
-    attributes.each {|key, value| self.send(("#{key}="), value)}
+  def initialize(attributes = {})
+    attributes.each  do |key, value| 
+      define_singleton_method("#{attr}=") { |val| attributes[attr] = val }
+      define_singleton_method(attr) { attributes[attr] }
   end
 
 end
